3084b96187680d0d504b4f87f582bd9c012180e0
Dhanush MC <dhanushmc93@gmail.com>
2023-11-10 19:58:38 +0530

    c

--- Routes/displayData.js
+++ Routes/displayData.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 const jwtSecert = "Myname is dhanush mc i'm fullstack developer";
 // const bcrypt = require("bcrypt");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
 router.post(
   "/createuser",
