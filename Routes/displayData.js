581ea1549997f349fbca8f595de8160c6b0669a9
Dhanush MC <dhanushmc93@gmail.com>
2023-11-10 16:27:02 +0530

    c

--- Routes/displayData.js
+++ Routes/displayData.js
@@ -35,44 +35,44 @@ const jwtSecert = "Myname is dhanush mc i'm fullstack developer";
 //   }
 // );
 
-router.post(
-  "/loginuser",
-  [
-    body("email").isEmail(),
-    body("password", "Incorrect Password").isLength({ min: 5 }),
-  ],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    let email = req.body.email;
-    try {
-      let userData = await User.findOne({ email });
-      if (!userData) {
-        return res
-          .status(400)
-          .json({ errors: "Tyr loggin with correct creandtials" });
-      }
-      const pwdCompare = await bcrypt.compare(
-        req.body.password,
-        userData.password
-      );
-      if (!pwdCompare) {
-        return res
-          .status(400)
-          .json({ errors: "Tyr loggin with correct creandtials" });
-      }
-      const data = {
-        user: { id: userData.id },
-      };
-      const authToken = jwt.sign(data, jwtSecert);
-      return res.json({ success: true, authToken: authToken });
-    } catch (error) {
-      console.log(error);
-      res.json({ success: false });
-    }
-  }
-);
+// router.post(
+//   "/loginuser",
+//   [
+//     body("email").isEmail(),
+//     body("password", "Incorrect Password").isLength({ min: 5 }),
+//   ],
+//   async (req, res) => {
+//     const errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(400).json({ errors: errors.array() });
+//     }
+//     let email = req.body.email;
+//     try {
+//       let userData = await User.findOne({ email });
+//       if (!userData) {
+//         return res
+//           .status(400)
+//           .json({ errors: "Tyr loggin with correct creandtials" });
+//       }
+//       const pwdCompare = await bcrypt.compare(
+//         req.body.password,
+//         userData.password
+//       );
+//       if (!pwdCompare) {
+//         return res
+//           .status(400)
+//           .json({ errors: "Tyr loggin with correct creandtials" });
+//       }
+//       const data = {
+//         user: { id: userData.id },
+//       };
+//       const authToken = jwt.sign(data, jwtSecert);
+//       return res.json({ success: true, authToken: authToken });
+//     } catch (error) {
+//       console.log(error);
+//       res.json({ success: false });
+//     }
+//   }
+// );
 
 module.exports = router;
