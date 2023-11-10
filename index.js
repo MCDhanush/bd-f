32a80ecc3aec77b98b1111fd07542b98805b73db
Dhanush MC <dhanushmc93@gmail.com>
2023-11-10 13:36:42 +0530

    commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+const PORT = 9000;
+app.get("/", function (request, response) {
+  response.send("🙋‍♂️, 🌏 🎊✨🤩");
+});
+
+app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
