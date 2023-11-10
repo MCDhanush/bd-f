d6c68072e2b0e567f6d6429a5240a6a603d3d2f2
Dhanush MC <dhanushmc93@gmail.com>
2023-11-10 14:38:58 +0530

    commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const mongoDB = require("./db");
+const mongoDB = require("./db.js");
 const cors = require("cors");
 
 mongoDB();
@@ -13,17 +13,18 @@ const corsConfig = {
 app.use(cors(corsConfig));
 app.options("", cors(corsConfig));
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://jazzy-piroshki-fa94ee.netlify.app/"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://jazzy-piroshki-fa94ee.netlify.app/"
+//   );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin,X-Requested-With,Content-Type,Accept"
+//   );
+//   next();
+// });
+
 const PORT = 9000;
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
