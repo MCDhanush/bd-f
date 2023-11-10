8f15d56aa18ac5489688cd427768fbd3557f7b47
Dhanush MC <dhanushmc93@gmail.com>
2023-11-10 14:25:17 +0530

    commit

---
+++ Routes/displayData.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../model/user.model.js");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwtSecert = "Myname is dhanush mc i'm fullstack developer";
+
+router.post(
+  "/createuser",
+  // we use express validator for CreateUser on line 9,10(on the line 4 we import that )ensure to remember this.
+  body("email").isEmail(),
+  body("name").isLength({ min: 5 }),
+  body("password", "Incorrect Password").isLength({ min: 5 }),
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions (also express val)
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const salt = await bcrypt.genSalt(10);
+    let secPassword = await bcrypt.hash(req.body.password, salt);
+    try {
+      await User.create({
+        name: req.body.name,
+        password: secPassword,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+
+router.post(
+  "/loginuser",
+  [
+    body("email").isEmail(),
+    body("password", "Incorrect Password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    let email = req.body.email;
+    try {
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res
+          .status(400)
+          .json({ errors: "Tyr loggin with correct creandtials" });
+      }
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password
+      );
+      if (!pwdCompare) {
+        return res
+          .status(400)
+          .json({ errors: "Tyr loggin with correct creandtials" });
+      }
+      const data = {
+        user: { id: userData.id },
+      };
+      const authToken = jwt.sign(data, jwtSecert);
+      return res.json({ success: true, authToken: authToken });
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const express = require("express");
 const app = express();
 const mongoDB = require("./db");
+const cors = require("cors");
+
 mongoDB();
 
+const corsConfig = {
+  origin: "",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
+
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://jazzy-piroshki-fa94ee.netlify.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept"
+  );
+  next();
+});
 const PORT = 9000;
 app.get("/", function (request, response) {
   response.send("🙋‍♂️, 🌏 🎊✨🤩");
 });
+
+app.use(express.json());
+app.use("/api", require("./Routes/user.controller.js"));
+app.use("/api", require("./Routes/displayData.js"));
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
