02b425e8b01e81d73f8e45a7dd9280257cee87ad
Dhanush MC <dhanushmc93@gmail.com>
2023-11-11 08:42:56 +0530

    c

---
+++ model/order.model.js
@@ -0,0 +1,14 @@
+const moongose = require("moongose");
+const { Schema } = moongose;
+
+const OrderSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  order_data: {
+    type: Array,
+    required: true,
+  },
+});
